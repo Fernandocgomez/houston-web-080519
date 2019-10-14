728dfc4f6c6c5b84a68512c6d563681e902752a8
joshua-miles <joshua@christetulit.org>
2019-10-14 11:06:38 -0500

    -

---
+++ 57-react-router/airlines-with-router/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PassengerIndex } from './PassengerIndex/PassengerIndex';
+import { PassengerShow } from './PassengerShow/PassengerShow';
+import { BrowserRouter, Route } from 'react-router-dom' 
+
+class App extends React.Component {
+
+  state = {
+    id: null
+  }
+
+  setId =  id => {
+    this.setState({ id: id })
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route exact path="/" component={PassengerIndex } />
+          <Route exact path="/passengers/:id" component={PassengerShow} />
+        </BrowserRouter>
+      </div>
+    );
+  }
+
+}
+
+export default App;
